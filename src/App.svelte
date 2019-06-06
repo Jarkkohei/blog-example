1537d569ad70cc7cb88950ddde12bc2e77800636
Jarkko <jarkkohei@luukku.com>
2019-06-06 19:25:46 +0300

    Add Home- and About-pages and add Router including routes to them to the App-component

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,23 @@
 <script>
 	import '../node_modules/materialize-css/dist/css/materialize.min.css';
 	import '../node_modules/materialize-css/dist/js/materialize.min.js';
+	
+	import { Router, Route } from 'svelte-routing';
 
 	import NavBar from './layout/NavBar.svelte';
+	import Home from './pages/Home.svelte';
+	import About from './pages/About.svelte';
+
 </script>
 
 <style>
 	
 </style>
 
-
+<Router>
+	<NavBar />
+	<div class="container">
+		<Route path="/" component={Home}></Route>
+		<Route path="/about" component={About}></Route>
+	</div>
+</Router>
