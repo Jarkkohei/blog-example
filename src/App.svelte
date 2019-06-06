ac78da6652296161542ec48fc68b3c3dd9e016c8
Jarkko <jarkkohei@luukku.com>
2019-06-06 19:21:13 +0300

    Add NavBar -component and import it to App-component

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script>
 	import '../node_modules/materialize-css/dist/css/materialize.min.css';
 	import '../node_modules/materialize-css/dist/js/materialize.min.js';
+
+	import NavBar from './layout/NavBar.svelte';
 </script>
 
 <style>
