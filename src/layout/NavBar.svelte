cef45e2460346b6eafcaaf35e52ff4a5fac17159
Jarkko <jarkkohei@luukku.com>
2019-06-06 23:02:49 +0300

    Add NavLink-component for getting the active link

--- src/layout/NavBar.svelte
+++ src/layout/NavBar.svelte
@@ -1,15 +1,16 @@
 <script>
     import { Link } from 'svelte-routing';
+    import NavLink from '../components/NavLink.svelte';
 </script>
 
 <nav>
-    <div class="nav-wrapper">
-      <div class="container">
-        <Link to="/"><span class="brand-logo">Blog-example</span></Link>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-      </div>
+  <div class="nav-wrapper">
+    <div class="container">
+      <Link to="/"><span class="brand-logo">Blog-example</span></Link>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li class="link-wrapper"><NavLink to="/">Home</NavLink></li>
+        <li class="link-wrapper"><NavLink to="/about">About</NavLink></li>
+      </ul>
     </div>
+  </div>
 </nav>
